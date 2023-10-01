573387e9844fec94991ad4b00e33dd5b15297801
Yi Xian <exian0716@gmail.com>
2023-10-01 18:57:06 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import Hero from './components/Hero';
-import Demo from './components/Demo';
-import './App.css';
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 
-const app = () => {
+import "./App.css";
+
+const App = () => {
   return (
     <main>
-        <div className='main'>
-            <div className='gradient' />
-        </div>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
 
-        <div className='app'>
-            <Hero />
-            <Demo />
-        </div>
+      <div className='app'>
+        <Hero />
+        <Demo />
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default app
+export default App;
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,15 +1,28 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
+
 
 const Demo = () => {
-  const[article, setarticle] = useState({
+  const[article, setArticle] = useState({
     url:'',
     summary:'',
   });
   
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
   const handleSubmit = async(e) => {
-    alert('Submitted');
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
     
+    if(data?.summary) {
+        const newArticle = { ...article, summary: data.summary };
+
+        setArticle(newArticle);
+
+        console.log(newArticle);
+    }
   }
 
   return (
