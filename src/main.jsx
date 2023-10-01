936a02b22073271ba0ff4233fdca66d7ba1f08b0
Yi Xian <exian0716@gmail.com>
2023-10-01 17:24:22 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
